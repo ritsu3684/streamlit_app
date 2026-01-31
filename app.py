e59873fd58e17a736ceead14c5a47dabb33676ff
ritsu3684 <T125080@ed.sus.ac.jp>
2026-01-31 22:24:34 +0900

    app.pyに辞書を追加

--- app.py
+++ app.py
@@ -15,27 +15,27 @@ with st.sidebar:
     if type == '海面':
         fish = st.multiselect('売上を確認したい漁業を選択してください（複数選択可）',
                               ['漁船漁業','捕鯨業','海面養殖業'])
-        if '漁船漁業' in fish:
-            fish = '漁業（海面）'
-        elif '捕鯨業' in fish:
-            fish = '捕鯨業'
-        elif '海面養殖業' in fish:
-            fish = '養殖業（海面）'
+        
+        mapping = {'漁船漁業':'漁業（海面）',
+                   '捕鯨業':'捕鯨業',
+                   '海面養殖業':'養殖業（海面）'}
+        
+        fish = [mapping[f] for f in fish]
 
     elif type == '内水面':
         fish = st.multiselect('売上を確認したい漁業を選択してください（複数選択可）',
                               ['内水面漁業','内水面養殖業'])
-        if '内水面漁業' in fish:
-            fish = '漁業（内水面）'
-        elif '内水面養殖業' in fish:
-            fish = '養殖業（内水面）'
-    
+        mapping = {'内水面漁業':'漁業（内水面）',
+                   '内水面養殖業':'養殖業（内水面）'}
+
+        fish = [mapping[f] for f in fish]
+
     elif type == '栽培':
         fish = st.multiselect('売上を確認したい漁業を選択してください（複数選択可）',
                               ['海面養殖業','内水面養殖業'])
-        if '海面養殖業' in fish:
-            fish = '海面養殖業'
-        elif '内水面養殖業' in fish:
-            fish = '内水面養殖業'
+        
+        mapping = {'海面養殖業':'養殖業（海面）',
+                   '内水面養殖業':'養殖業（内水面）'}
     
-    # 現状elifでは複数条件を選択しても1つしか条件をしぼれない
+        fish = [mapping[f] for f in fish]
+   
