c74584028345f512c28256faa161c08741af8756
ritsu3684 <T125080@ed.sus.ac.jp>
2026-02-01 00:55:48 +0900

    a

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 # タイトル
 st.title('各年における漁業産出額')
 
-df = pd.read_csv('漁業産出額.csv',engine='python')
-
+df = pd.read_csv('漁業産出額.csv',na_values=['未計測'],skipinitialspace=True)
+df.columns = df.columns.str.strip()
 # サイドバー
 # 漁業の種類によってマルチセレクトを変更
 with st.sidebar:
